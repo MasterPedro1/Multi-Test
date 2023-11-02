dadb7d11cb14628c866e21d07bcc9191630b7862
LDom890 <pedromoralescaba@gmail.com>
2023-11-01 20:20:29 -0600

    trabajo terminado

--- 2D_Versus_Mult/Assets/Master/Scripts/Player_Controller.cs
+++ 2D_Versus_Mult/Assets/Master/Scripts/Player_Controller.cs
@@ -1,29 +1,69 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player_Controller : FSM
 {
-    [Header("Player_Settings")]
-    public float velocidad = 5.0f;
-    [SerializeField] float JumpForce;
-    [SerializeField] float attackRadius;
-    [SerializeField] bool isplayerDead;
-    public enum FSMState
-    {
-        None, Idle, Moving, Jump, Fall, Attack, Hit, Dead
-    }
-    public FSMState curState = FSMState.None;
+    public float velocidadMovimiento = 5f; // Velocidad de movimiento del jugador
+    public float fuerzaSalto = 10f; // Fuerza de salto
+    private bool enSuelo = false; // Verificar si el jugador esta en el suelo
+    private Rigidbody2D rb;
+    private bool isLocalPlayer = true;
 
-    void Update()
+    protected override void initialize()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected override void FSMUpdate()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        else
+        {
+            // Mover el jugador
+            MoverJugador();
+
+            // Saltar si se presiona la tecla de espacio y el jugador est� en el suelo
+            if (enSuelo && Input.GetKeyDown(KeyCode.Space))
+            {
+                Saltar();
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Verificar si el jugador est� en contacto con la capa "Suelo"
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Suelo"))
+        {
+            enSuelo = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        // Verificar si el jugador ya no est� en contacto con la capa "Suelo"
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Suelo"))
+        {
+            enSuelo = false;
+        }
+    }
+
+    void MoverJugador()
     {
         float movimientoHorizontal = Input.GetAxis("Horizontal");
-        float movimientoX = movimientoHorizontal * velocidad * Time.deltaTime;
-        transform.Translate(new Vector3(movimientoX, 0, 0));
+        Vector2 velocidad = rb.velocity;
+        velocidad.x = movimientoHorizontal * velocidadMovimiento;
+        rb.velocity = velocidad;
     }
-    private void FixedUpdate()
+
+    void Saltar()
     {
-        
+        rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
     }
 }
